989ff76cbbf46e3e8ce901e87b47415a9b53ed0b
Nathaly-D <113744119+Nathaly-D@users.noreply.github.com>
2024-11-23 23:24:54 -0500

    First test

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const icon = require('./assets/LogoGux.png');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={icon} style={{width:100, height:100}}/>
+      <Text>Inicio de Sesión</Text>
       <StatusBar style="auto" />
     </View>
   );
